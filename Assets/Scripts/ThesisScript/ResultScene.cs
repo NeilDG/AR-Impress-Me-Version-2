e1a136e8a23fc2c39e0b706d8200f537c8c395d4
fweezee <jordangana23@gmail.com>
2019-04-01 03:02:54 +0800

    result screen

---
+++ Assets/Scripts/ThesisScript/ResultScene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultScene : MonoBehaviour
+{
+
+    [SerializeField] private GameObject save_button;
+    [SerializeField] private GameObject retry_button;
+    [SerializeField] private Image result_image;
+    private Sprite screenshot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayImage(Application.persistentDataPath + "/UnalteredScene.png");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SaveImage()
+    {
+
+    }
+
+    private void DisplayImage(string path)
+    {
+        if (System.IO.File.Exists(path))
+        {
+            Debug.Log("3");
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+            Texture2D texture = new Texture2D(1, 1);
+            texture.LoadImage(bytes);
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100);
+            result_image.sprite = sprite;
+            screenshot = sprite;
+            Debug.Log("4");
+        }
+    }
+}
